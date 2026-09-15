a5a7047f1e9f1108ef1f940323a3ffb57f04a499
Jv7Pinheiro <deolivj@purdue.edu>
2026-09-14 17:01:49 -0700

    cavemen merging

--- hamiltonians.py
+++ hamiltonians.py
@@ -27,6 +27,26 @@ def _diagonal_8x8():
     ],dtype=float)
 
 
+def _diagonal_16x16():
+    return np.array([
+        [16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    ],dtype=float)
+
 HAMILTONIANS = {
     "belldiagonal": _belldiagonal_4x4,
     "belldiagonal4x4": _belldiagonal_4x4,
@@ -34,6 +54,8 @@ HAMILTONIANS = {
     "diagonal_8x8": _diagonal_8x8,
     "belldiagonal16x16": _belldiagonal_16x16,
     "belldiagonal_16x16": _belldiagonal_16x16,
+    "diagonal16x16": _diagonal_16x16,
+    "diagonal_16x16": _diagonal_16x16,
 }
 
 
@@ -53,6 +75,8 @@ def get_hamiltonian(name):
         "diagonal_8x8": "diagonal8x8",
         "belldiagonal16x16": "belldiagonal16x16",
         "belldiagonal_16x16": "belldiagonal16x16",
+        "diagonal16x16": "diagonal16x16",
+        "diagonal_16x16": "diagonal16x16",
     }.get(normalized, normalized)
 
     if canon not in HAMILTONIANS:
